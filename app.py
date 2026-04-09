d8d09136b7c7f9e9d272ce2edc3945e1025847f8
Bibek <bibek.joshi91@gmail.com>
2026-04-09 07:27:25 +0545

    final project push

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
 import streamlit as st
 import pandas as pd
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import pickle
+
+
+nltk.download('stopwords')
+words = stopwords.words("english")
+stemmer = PorterStemmer()
+
 
 
 st.title('Streamlit Project')
-
-
-
 st.write("News Classification")
+
+
+# Open the file in binary mode for reading
+with open('LogisticRegression.pickle', 'rb') as file:
+    # Load the data from the file
+    model = pickle.load(file)
+
+
+
 
 #taking input from users
 data  = st.text_area("Enter New for the Classification")
 
 if st.button('Submit'):
-
-    d = {"news": [data]}
+    d = {"predict_news": [data]}
     df = pd.DataFrame(d)
-    st.write(df)
+    df['predict_news'] = list(map(lambda x: " ".join([i for i in x.lower().split() if i not in words]), df['predict_news']))
+    df['predict_news'] = df['predict_news'].apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z]", " ", x).split() if i not in words]).lower())
+    
+    predict_news_cat = model.predict(df['predict_news'])
+    st.write(predict_news_cat)
+    
+    st.write(predict_news_cat)
